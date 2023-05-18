6dbf75495af6b2a3c4e2aae2ebf192ea1191f1ce
Igor-Lysak <lisakigor485@gmail.com>
2023-05-18 21:05:56 +0300

    updateDz

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./ContactForm.module.css";
 
-const ContactForm = ({ onAddContact, onCheck }) => {
+const ContactForm = ({ onAddContact, value }) => {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
 
@@ -17,18 +17,18 @@ const ContactForm = ({ onAddContact, onCheck }) => {
       <h3>Name</h3>
       <form onSubmit={handleSubmit}>
         <input
+          value={name}
           onInput={(e) => setName(e.target.value)}
           type="text"
-          name={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
         <h3>Phone Number</h3>
         <input
+          value={number}
           onInput={(e) => setNumber(e.target.value)}
           type="tel"
-          name={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
