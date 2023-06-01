fe8b04b603df6ef61caf2ea4a52099180854bacf
Igor-Lysak <lisakigor485@gmail.com>
2023-06-01 21:10:23 +0300

    updatelocalstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import styles from "./App.module.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Filter from "./components/Filter/Filter";
 import ContactList from "./components/ContactList/ContactList";
 
+function initContacts() {
+  const contacts = localStorage.getItem("contacts");
+  if (contacts) {
+    return JSON.parse(contacts);
+  } else {
+    return [];
+  }
+}
+
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(initContacts);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   function addContact(name, number) {
     const newContact = {
